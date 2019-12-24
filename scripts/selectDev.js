a059a9c0bb2b9f78bcb9cd05b16b201fdc78bf4f
gabrielEloy <gabrieleloy@pop-os.localdomain>
2019-12-24 18:51:56 -0300

    page taking shape

--- scripts/selectDev.js
+++ scripts/selectDev.js
@@ -7,7 +7,7 @@ function updateData() {
     const devName = document.getElementById('dev-name');
     const devTitle = document.getElementById('dev-title');
     const picture = document.getElementById('dev-pic');
-    const desc = document.getElementById('desccription');
+    const desc = document.getElementById('description');
 
     devName.innerText = selectedDev;
     devTitle.innerText = 'Desenvolvedor';
@@ -15,11 +15,24 @@ function updateData() {
     desc.innerText = selectedDev === 'Victor Eloy' ? 'Lorem ipsum Veloy;' : 'Lorem ipsum Geloy;'
 }
 
+function retriggerAnimation(elementID, animationClass) {
+    const devPic = document.getElementById(elementID);
+    devPic.classList.remove(animationClass);
+    void devPic.offsetWidth;
+
+    devPic.classList.add(animationClass);
+
+}
+
 for(let dev of devs){
     dev.addEventListener('click', (e) => {
         if(!isDown){
             selectedDev = dev.outerText.trim();
             updateData();
+            retriggerAnimation('dev-pic', 'slide-animation');
+            retriggerAnimation('description', 'show-animation');
+            retriggerAnimation('dev-name', 'drop-animation');
+            retriggerAnimation('dev-title', 'slide-left-animation');
         }
     })
 }
